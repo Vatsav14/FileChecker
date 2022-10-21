206fa60d0fd01388ff13013a2a3fd6bcf97688dd
Vatsav <vatsav14@outlook.com>
2022-10-21 13:26:34 -0700

    Changed timestamp to ISO 8601

---
+++ build/CMakeFiles/fileChecker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fileChecker.dir/fileChecker.cpp.o"
+  "fileChecker"
+  "fileChecker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fileChecker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/publish/CMakeFiles/publish.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/publish.dir/publish.cpp.o"
+  "libpublish.a"
+  "libpublish.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/publish.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
